4d0f26ed2250916d66b41ec73aaa122b33767a16
Nikita Kuznetsov <nikita0bond@gmail.com>
2021-03-31 13:16:13 +0300

    fix tests

--- modules/complex-number/test/test_kuznetsov_nikita_complex_number.cpp
+++ modules/complex-number/test/test_kuznetsov_nikita_complex_number.cpp
@@ -1,10 +1,12 @@
 // Copyright 2021 Kuznetsov Nikita
 
 #include <gtest/gtest.h>
+
 #include <cmath>
+#include <tuple>
 #include "include/complex_number.h"
 
-TEST(Kuznetsov_Nikita_ComplexNumberTest, Can_Assign_Element) {
+TEST(Kuznetsov_Nikita_ComplexNumberTest, Can_Copy_Element) {
     double re = 3.0;
     double im = 2.0;
     ComplexNumber z1(re, im);
@@ -15,20 +17,32 @@ TEST(Kuznetsov_Nikita_ComplexNumberTest, Can_Assign_Element) {
     ASSERT_TRUE(sw);
 }
 
-TEST(Kuznetsov_Nikita_ComplexNumberTest, Can_Sum_Elements) {
-  ComplexNumber z1(3.0, 2.0);
-  ComplexNumber z2(1.0, 3.0);
+typedef testing::TestWithParam<std::tuple<double, double>>
+Kuznetsov_Nikita_ComplexNumberTest_param;
+TEST_P(Kuznetsov_Nikita_ComplexNumberTest_param, Can_Sum_Elements) {
+  double re = std::get<0>(GetParam());
+  double im = std::get<1>(GetParam());
+  ComplexNumber z1(re, im);
+  ComplexNumber z2(re, im);
 
   ComplexNumber sum = z1 + z2;
 
-  EXPECT_EQ(sum, ComplexNumber(4.0, 5.0));
+  ASSERT_EQ(sum, ComplexNumber(2 * re, 2 * im));
 }
 
-TEST(Kuznetsov_Nikita_ComplexNumberTest, Can_Multiply_Elements) {
-  ComplexNumber z1(3.0, 2.0);
-  ComplexNumber z2(1.0, 3.0);
+TEST_P(Kuznetsov_Nikita_ComplexNumberTest_param, Can_Multiply_Elements) {
+  double re = std::get<0>(GetParam());
+  double im = std::get<1>(GetParam());
+  ComplexNumber z1(re, im);
+  ComplexNumber z2(re, im);
 
   ComplexNumber result = z1 * z2;
 
-  EXPECT_EQ(result, ComplexNumber(-3.0, 11.0));
+  ASSERT_EQ(result, ComplexNumber(re * re - im * im, re * im + im * re));
 }
+
+INSTANTIATE_TEST_CASE_P(/**/, Kuznetsov_Nikita_ComplexNumberTest_param,
+  testing::Combine(
+    testing::Values(-10.0, 2.0),
+    testing::Values(5.0, -4.0)
+  ));

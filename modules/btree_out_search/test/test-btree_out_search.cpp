733624453bd00776200b970fad6a1e5b36fd4a16
Nikita Kuznetsov <nikita0bond@gmail.com>
2021-04-24 20:06:07 +0300

    add more tests

--- modules/btree_out_search/test/test-btree_out_search.cpp
+++ modules/btree_out_search/test/test-btree_out_search.cpp
@@ -13,6 +13,13 @@ TEST(BINARY_TREE, ADD_VALUE_TO_THE_ROOT) {
   EXPECT_NO_THROW(root = addtree(root, 12));
 }
 
+TEST(BINARY_TREE, ADD_VALUE_AFTER_THE_ROOT) {
+  struct tnode* root;
+  root = NULL;
+  root = addtree(root, 12);
+  EXPECT_NO_THROW(root = addtree(root, 8));
+}
+
 TEST(BINARY_TREE, CAN_SEARCH) {
   tnode* root;
   root = NULL;
@@ -28,6 +35,33 @@ TEST(BINARY_TREE, CAN_SEARCH_WHEN_NOT_FOUND) {
   root = NULL;
   int search_value = -15;
   for (int i = 0; i < 100; i++) {
+    root = addtree(root, i);
+  }
+  EXPECT_TRUE(NULL == find(root, search_value));
+}
+
+TEST(BINARY_TREE, CANT_SEARCH_WHEN_TREE_IS_EMPTY) {
+  tnode* root;
+  root = NULL;
+  int search_value = -15;
+  EXPECT_TRUE(NULL == find(root, search_value));
+}
+
+TEST(BINARY_TREE, CAN_SEARCH2) {
+  tnode* root;
+  root = NULL;
+  int search_value = 50;
+  for (int i = 100; i > 0; i--) {
+    root = addtree(root, i);
+  }
+  EXPECT_TRUE(NULL != find(root, search_value));
+}
+
+TEST(BINARY_TREE, CAN_SEARCH_WHEN_NOT_FOUND2) {
+  tnode* root;
+  root = NULL;
+  int search_value = -50;
+  for (int i = 100; i > 0; i--) {
     root = addtree(root, i);
   }
   EXPECT_TRUE(NULL == find(root, search_value));
